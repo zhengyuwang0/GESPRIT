0af21b825ba9dacad9c8a724d98582b1aaf0191d
zhengyuwang0 <168277179+zhengyuwang0@users.noreply.github.com>
2025-01-04 22:07:12 +0800

    Update Fig4.m

--- Fig4.m
+++ Fig4.m
@@ -1,16 +1,24 @@
 clear; clc;
 N = 400;
 T  = 800;
-c = N/T;   % sqrt(0.25)=0.5
+c = N/T;   
 nb_Loop = 50;
-% n = N-1;
-% delta = 1;
-n = floor(2*N/3);
-delta = floor(N/3);
-theta_true = [0,0.8*2*pi/N];
-% theta_true = [0,pi/4];
-k = length(theta_true);
+testcase = 'widely';  % or closely
 
+switch testcase
+    case 'widely'
+        theta_true = [0,pi/4];
+        n = N-1;
+        delta = 1;
+        P1 = [2,0.8;0.8,2];
+    case 'closely'
+        theta_true = [0,0.8*2*pi/N];
+        n = floor(2*N/3);
+        delta = floor(N/3);
+        P1 = 2*[1,0;0,1];
+end
+
+k = length(theta_true);
 
 clear i
 a = @(theta) exp(1i*theta*(0:N-1)')/sqrt(N);
@@ -22,16 +30,16 @@ for tmp_index=1:length(theta_true)
     diffA = [diffA diffa(theta_true(tmp_index))];
 end
 Df = diffA;
-P1 = 2*[1,0;0,1];
+
 [V,E] = eig(A*P1*A','vector');
 [E, index1] = sort(E,'descend');
 V = V(:, index1);
 V = V(:,1:2);
 
-SNRList = -4:2:14;
+SNRList = -8:2:12;
 powList = db2pow(SNRList)/E(2);  
 sigma2 = 1;
-SNR_cond = pow2db(sqrt(c)/E(2));
+SNR_cond = pow2db(sqrt(c)/E(2))
 
 ESPRIT_MSE = zeros(nb_Loop,length(SNRList));
 MUSIC_MSE = zeros(nb_Loop,length(SNRList));
@@ -53,12 +61,15 @@ for it=1:1:length(SNRList)
         [eigs_SCM, index] = sort(eigs_SCM,'descend');
         U = U(:, index);
         U_S = U(:,1:k);
+        
+%         gg_true = (1-c*rho.^(-2))./(1+c*rho.^(-1));   % 2*1
+%         kk_true = n/N*(c+c*rho.^(-1))./(c+rho);
 
         lambda_bar = eigs_SCM(1:k);
         if lambda_bar>=(1+sqrt(c))^2
             ell_estim = (lambda_bar-(1+c))/2 + sqrt((lambda_bar-(1+c)).^2 - 4*c)/2;
             gg = (1-c*ell_estim.^(-2))./(1+c*ell_estim.^(-1));  
-            kk = n/N*(c+c*ell_estim.^(-1))./(c+ell_estim);
+            kk = 1-gg;
         else
             gg = ones(2,1); kk = zeros(2,1);
         end
@@ -66,12 +77,11 @@ for it=1:1:length(SNRList)
         MUSIC_Theta = GetMusic(U_S);
         GMUSIC_Theta = GetGMusic(U_S,eigs_SCM,c);
         ESPRIT_Theta = GetESPRITE(U_S,n,delta);   % 2*1
-        MLE_Theta = GetMLE(T,N,X);   % 2*1
         GESPRIT_Theta = GetGESPRITE(U_S,n,delta,gg,kk);   % 2*1
-        MUSIC_MSE(jj,it) = (MUSIC_Theta(2) - theta_true(2)).^2;
-        GMUSIC_MSE(jj,it) = (GMUSIC_Theta(2) - theta_true(2)).^2;
-        ESPRIT_MSE(jj,it) =(ESPRIT_Theta(2) - theta_true(2)).^2;
-        GESPRIT_MSE(jj,it) =(GESPRIT_Theta(2) - theta_true(2)).^2;
+        MUSIC_MSE(jj,it) = sum((MUSIC_Theta - theta_true).^2)/2;
+        GMUSIC_MSE(jj,it) = sum((GMUSIC_Theta - theta_true).^2)/2;
+        ESPRIT_MSE(jj,it) =sum((ESPRIT_Theta.' - theta_true).^2)/2;
+        GESPRIT_MSE(jj,it) =sum((GESPRIT_Theta.' - theta_true).^2)/2;
 
     end
     ESPRIT_MSE_E  = mean(ESPRIT_MSE,1);
@@ -80,8 +90,7 @@ for it=1:1:length(SNRList)
     GESPRIT_MSE_E  = mean(GESPRIT_MSE,1);
 
     CRB = sigma2 / (2*T) *inv(real(Df'*(eye(N)-A*inv(A'*A)*A')*Df) .*P);
-    % CRB_Theory(1,it) = trace(CRB)/k;
-    CRB_Theory(1,it) = CRB(2,2);
+    CRB_Theory(1,it) = trace(CRB)/k;
 end
 
 figure();
@@ -96,8 +105,3 @@ hold off;
 legend('MUSIC MSE','GMUSIC MSE','ESPRIT MSE','GESPRIT MSE','CRB')
 xlabel('N')
 ylabel('log10')
-
-
-
-
-
